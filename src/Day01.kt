0d0caac8558f26d2cfe77f04a74ead25e5681f62
ledeni56 <lovro.ludvig@gmail.com>
2023-12-01 11:27:01 +0100

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,56 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun Pair<String, String>.replacement() = second + first.substring(1)
+
+    fun findCalibrationCodePartOne(input: List<String>): Long {
+        // Calculate the code
+        var sum = 0L
+        input.map { calibrationLine ->
+            var firstDigit: Char? = null
+            var lastDigit: Char? = null
+            for (index in calibrationLine.indices) {
+                calibrationLine[index].takeIf { it.isDigit() }?.let { digit ->
+                    if (firstDigit == null) firstDigit = digit
+                    lastDigit = digit
+                }
+            }
+            sum += ("$firstDigit$lastDigit").toInt()
+        }
+        return sum
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun findCalibrationCodePartTwo(input: List<String>): Long {
+        // Mapper
+        val mapper = arrayOf(
+            "one" to "1",
+            "two" to "2",
+            "three" to "3",
+            "four" to "4",
+            "five" to "5",
+            "six" to "6",
+            "seven" to "7",
+            "eight" to "8",
+            "nine" to "9",
+        )
+        // Remap values
+        val adjustedInput = input.map { oldCalibrationLine ->
+            var newCalibrationLine = oldCalibrationLine
+            for (index in oldCalibrationLine.indices) {
+                mapper.forEach { mapperPair ->
+                    val substring = oldCalibrationLine.substring(index)
+                    if (substring.startsWith(mapperPair.first)) {
+                        newCalibrationLine = newCalibrationLine.replace(
+                            oldValue = substring,
+                            newValue = substring.replaceFirst(mapperPair.first, mapperPair.replacement()),
+                        )
+                    }
+                }
+            }
+            newCalibrationLine
+        }
+        return findCalibrationCodePartOne(adjustedInput)
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val calibrationDocument = readInput("Day_01_calibration")
+    println(findCalibrationCodePartOne(calibrationDocument))
+    println(findCalibrationCodePartTwo(calibrationDocument))
 }
